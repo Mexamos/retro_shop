1c81d2e4a1f5c477b1cd5c71dc682d5138393be4
Maksim <tolstikov_m@mail.ru>
2019-07-04 22:32:46 +0300

    Add correct images, add offers to page, script that add offers to cart

--- shop/models.py
+++ shop/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Offers(models.Model):
     name = models.CharField(null=True, blank=True,max_length=100, verbose_name='Название')
     description = models.TextField(null=True, blank=True, verbose_name='Описание')
-    image = models.ImageField(verbose_name='Изображение')
+    image = models.ImageField(upload_to="gallery", verbose_name='Изображение')
     price = models.FloatField(null=True, blank=True, verbose_name='Цена')
 
     class Meta:
