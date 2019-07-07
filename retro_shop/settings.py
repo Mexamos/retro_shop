926ab39193c4e2f604fb8151529619000207fe13
Maksim <tolstikov_m@mail.ru>
2019-07-07 22:59:46 +0300

    Change setting's language

--- retro_shop/settings.py
+++ retro_shop/settings.py
@@ -108,7 +108,12 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
+
+LANGUAGES = (
+    ('ru', u'Русский'),
+    ('en', u'Английский'),
+)
 
 TIME_ZONE = 'UTC'
 
